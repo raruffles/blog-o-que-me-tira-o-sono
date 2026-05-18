48f73e9e429d2142592736cb4b9655ff2d3a8a3e
raphael de oliveira lima <raphaeldeoliveiralima@gmail.com>
2026-05-17 21:21:19 -0300

    fix: muda para modo hibrido e salva a home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 import keystatic from '@keystatic/astro';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid', // <--- Mude apenas esta linha!
   adapter: vercel(),
   integrations: [
     react(), 
+    tailwind(), 
     keystatic()
   ],
 });
-
-
