2ef435d091bac5d9ae01190abfd1e84e83ccc4e0
raphael de oliveira lima <raphaeldeoliveiralima@gmail.com>
2026-05-17 21:41:39 -0300

    teste: isola keystatic e muda preset da vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import keystatic from '@keystatic/astro';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'hybrid', // <--- Mude apenas esta linha!
+  output: 'server',
   adapter: vercel(),
   integrations: [
     react(), 
     tailwind(), 
-    keystatic()
+    // keystatic()  <-- Deixe comentado com as duas barras!
   ],
 });
